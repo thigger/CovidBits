51741069f70d0de315aac7e9dcad384874ce4d9a
Tom Lawton <tlawton@gmx.de>
2021-01-30 16:33:38 +0000

    Add graphs: xy graph of total cases vs HAI percentage xy graph of 4-hour performance (December only) vs HAI percentage

--- HAI.R
+++ HAI.R
@@ -99,3 +99,43 @@ ggp<-ggplot(acutes_grp_lim,aes(y=fct_reorder(casesName,percHAI),x=percHAI,fill=p
 
 
 ggp %>% ggplotly(tooltip="y")
+
+ggplot(acutes_grp_lim,aes(x=totCases,y=percHAI,colour=percHAI))+
+  geom_point()+
+  scale_color_gradient(low="blue",high="red",guide=FALSE)+
+  xlab("Cases since August 2020 (log)")+
+  scale_x_continuous(trans="log10")+
+  scale_y_continuous(labels=scales::percent) +
+  ylab("Percentage HAI") +
+  #geom_text(aes(label=totCases,x=0)) +
+  ggtitle("Hospital Acquired COVID-19")
+
+cor.test(acutes_grp_lim$totCases,acutes_grp_lim$percHAI)
+
+
+## Graph against 4-hr performance after a request by @DrLindaDykes
+## 4hr performance from:
+## using December only initially
+## https://www.england.nhs.uk/statistics/statistical-work-areas/ae-waiting-times-and-activity/ae-attendances-and-emergency-admissions-2020-21/
+
+cnames<-paste0("aeperf",read_excel("December-2020-AE-by-provider-8c90a.xls",sheet="Provider Level Data",n_max=0,skip=15) %>% names())
+cnames<-c(cnames,"aeperfSTP") #hidden column
+hosp_aeperf_wide<-na.omit(read_excel("December-2020-AE-by-provider-8c90a.xls",sheet="Provider Level Data",skip=18,col_names=cnames))
+
+test<-merge(acutes_grp_lim,hosp_aeperf_wide,by.x="casesCode",by.y="aeperfCode")
+
+test$`aeperfPercentage in 4 hours or less (all)`
+
+test$aeperfPerc4hr<-as.numeric(test$`aeperfPercentage in 4 hours or less (all)`)
+
+ggplot(test,aes(x=`aeperfPerc4hr`,y=percHAI,colour=percHAI))+
+  geom_point()+
+  scale_color_gradient(low="blue",high="red",guide=FALSE)+
+  xlab("December 2020 4-hour performance")+
+  scale_y_continuous(labels=scales::percent) +
+  ylab("Percentage HAI") +
+  #geom_text(aes(label=totCases,x=0)) +
+  ggtitle("Hospital Acquired COVID-19")
+
+cor.test(test$percHAI,test$aeperfPerc4hr,use="complete.obs")
+
